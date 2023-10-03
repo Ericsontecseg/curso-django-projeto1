d40e4a26a313568af792fc7c99383d81f0711efb
Ericson Carlos <ericsontecseg@yahoo.com.br>
2023-10-02 21:52:27 -0300

    Cont projeto

--- recipes/views.py
+++ recipes/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'Ericson Carlos'
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Ericson Carlos',
     })
 
 
-
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Ericson Carlos',
+    })
